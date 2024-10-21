2df4fbe188dd6b59cb9b5e2112afd454f474ef28
Asad Qayyum <asadqayyumbscs@gmail.com>
2024-10-21 23:21:10 +0500

    feat:fiexed the session stroage issue

--- src/app/shared/interceptors/guards/auth.guard.ts
+++ src/app/shared/interceptors/guards/auth.guard.ts
@@ -1,24 +1,27 @@
 import { Inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { Router } from '@angular/router';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, } from '@angular/common';
 import { AuthService } from '../../../auth.service';
-import {  PLATFORM_ID } from '@angular/core';
+import { inject } from '@angular/core';
+import { PLATFORM_ID } from '@angular/core';
+
 export const authGuard: CanActivateFn = (route, state) => {
- let router=new Router();
- let auth_servise=Inject(AuthService);
+    const router = inject(Router);
+    const authService = inject(AuthService);
+    const platformId:Object = inject(PLATFORM_ID);
 
-// const router=new myRouter();
-let token
-if(typeof sessionStorage !=undefined){
-   token=sessionStorage.getItem('jwtToken');
-}
+    let token;
 
-  if(token){
-   return true;
-  }
+    if (isPlatformBrowser(platformId)) {
+        // Only access sessionStorage in a browser context
+        token = sessionStorage.getItem('jwtToken');
+    }
 
-router.navigate(['login'])
-return false; 
+    if (token) {
+        return true; // Allow access
+    }
 
+    router.navigate(['login']);
+    return false; // Deny access
 };
